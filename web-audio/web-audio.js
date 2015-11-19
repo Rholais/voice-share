d0f27fd4c5e50f93d1aa08ba787bddec1b9b86f7
Rholais Lii <rholais@gmail.com>
2015-11-20 01:46:34 +0800

    Update web-audio.js
    Load a sound sample.

--- web-audio/web-audio.js
+++ web-audio/web-audio.js
@@ -2,10 +2,30 @@ var context;
 window.addEventListener('load', init, false);
 function init() {
 	try {
-		// Fix up for prefixing
-		window.AudioContext = window.AudioContext||window.webkitAudioContext;
-		context = new AudioContext();
-		alert('Web Audio API is supported in this browser');
+		var dogBarkingBuffer = null;
+		// Fix up prefixing
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		var context = new AudioContext();
+		
+		url = 'https://commons.wikimedia.org/wiki/File:Sound-of-dog.ogg';
+		function loadDogSound(url) {
+			var request = new XMLHttpRequest();
+			request.open('GET', url, true);
+			request.responseType = 'arraybuffer';
+			
+			// Decode asynchronously
+			request.onload = function() {
+				context.decodeAudioData(
+					request.response, 
+					function(buffer) {
+						dogBarkingBuffer = buffer;
+					}, 
+					onError
+				);
+			}
+			request.send();
+			alert('Request sent.');
+		}
 	}
 	catch(e) {
 		alert('Web Audio API is not supported in this browser');
