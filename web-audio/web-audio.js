58f36050a59970c71ea57669ae14286697a1ca8c
Rholais Lii <rholais@gmail.com>
2015-11-21 11:02:39 +0800

    Try `noteOn` method

--- web-audio/web-audio.js
+++ web-audio/web-audio.js
@@ -29,9 +29,9 @@ function loadDogSound(url) {
 }
 
 function playSound(buffer) {
-  var source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);                           // play the source now
-                                             // note: on older systems, may have to use deprecated noteOn(time);
+	var source = context.createBufferSource(); // creates a sound source
+	source.buffer = buffer;                    // tell the source which sound to play
+	source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+	source.start(0);                           // play the source now
+//	source.noteOn(0);						   // note: on older systems, may have to use deprecated noteOn(time);
 }
